7dea8ce7ea5c1d9b6da66dd862f7921d7bd7a029
ravi-1120 <75559056+ravi-1120@users.noreply.github.com>
2021-07-10 14:30:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-$(document).ready(function(){
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
 
-    $(document).scroll(function(){
-  
-      var footerSelector = '#footer';
-      var socialBarSelector = '.icon-bar';
-  
-      var bottomViewPort = $(window).scrollTop()+$(window).height();
-      var footerTop = $(footerSelector ).offset().top;
-  
-      if( bottomViewPort>=footerTop){
-        $(socialBarSelector).fadeOut();
-      }else{
-        $(socialBarSelector).fadeIn();
-      }
-    });
-  
-  });
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
+
